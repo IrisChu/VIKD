832f3406668dcf0fcdf0bca09c56e33fff9fdec4
Iris Chu <k7b8@bowen.ugrad.cs.ubc.ca>
2016-06-08 22:06:53 -0700

    Modified pokedex.php

--- pokedex.php
+++ pokedex.php
@@ -1,51 +1,22 @@
 <html>
 <?php
-$db_conn = OCILogon("ora_w6g0b", "a22262142", "ug");
-$success = True; //keep track of errors so it redirects the page only if there are no errors
+$db_conn = OCILogon("ora_k7b8", "a73488090", "ug");
 
-function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
-	//echo "<br>running ".$cmdstr."<br>";
-	global $db_conn, $success;
-	$statement = OCIParse($db_conn, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
-
-	if (!$statement) {
-		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-		$e = OCI_Error($db_conn); // For OCIParse errors pass the
-		// connection handle
-		echo htmlentities($e['message']);
-		$success = False;
-	}
-
-	$r = OCIExecute($statement, OCI_DEFAULT);
-	if (!$r) {
-		echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-		$e = oci_error($statement); // For OCIExecute errors pass the statementhandle
-		echo htmlentities($e['message']);
-		$success = False;
-	} else {
-
-	}
-	return $statement;
-
-}
-
-function printResult($result) { //prints results from a select statement
-	echo "<br>Got data from table TYPE:<br>";
+function printResult($result) {
+	echo "<br>FROM TABLE TYPE:<br>";
 	echo "<table>";
 
 	while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-		echo "<tr><td>" . $row['typeName'] . "</td></tr>";
+		echo "<tr><td>" . $row['TYPENAME'] . "</td></tr>";
 	}
 	echo "</table>";
+}
 
 if ($db_conn) {
-
-  $x = executePlainSQL("select * from type");
-  echo isset($x);
-  printResult($x);
-  //printResult($x);
-OCICommit($db_conn);
-
+  $sqlStmt = "select * from type";
+  $statement = OCIParse($db_conn, $sqlStmt); 
+  OCIExecute($statement);
+  printResult($statement);
 }
 OCILogoff($db_conn);
 
